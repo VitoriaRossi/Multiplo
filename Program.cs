ff962ec38bd57299ba45d06ddc0a1231c4fea728
VitoriaRossi <vitoria.rossi555@gmail.com>
2023-12-10 16:21:08 -0300

    Realização da atividade sobre verificar número múltiplo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("--- Múltiplo ---");
+
+Console.Write("Digite um número: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Verificar se é múltiplo de: ");
+int multiplier = Convert.ToInt32(Console.ReadLine());
+
+if (value % multiplier == 0) {
+  Console.WriteLine($"{value} é múltiplo de {multiplier}.");
+} else {
+  Console.WriteLine($"{value} não é múltiplo de {multiplier}.");
+}
